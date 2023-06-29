27a1e93b24b26a309386cf44de905e3348d09eea
Andre Vargas Roo <a.vargas252@icloud.com>
2023-06-29 18:02:15 -0400

    store unit test created and passed

--- src/Store/restaurants/reducers.js
+++ src/Store/restaurants/reducers.js
@@ -1,7 +1,13 @@
 import {combineReducers} from "redux"
+import { STORE_RESTAURANTS } from "./actions"
 
-function records() {
-    return []
+function records(s = [], action) {
+    switch (action.type) {
+        case STORE_RESTAURANTS:
+            return action.records
+        default:
+            return s
+    }
 }
 
 export default combineReducers({
