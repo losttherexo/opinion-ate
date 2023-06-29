5f2f37c98c88d2381b5c180b79fefaec0f0c33f1
Andre Vargas Roo <a.vargas252@icloud.com>
2023-06-29 18:14:00 -0400

    create api.js, implement axios

--- src/Store/index.js
+++ src/Store/index.js
@@ -2,7 +2,11 @@ import {legacy_createStore} from 'redux'
 import {applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
+import api from '../api'
 
-const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
+const store = legacy_createStore(
+    rootReducer,
+    applyMiddleware(thunk.withExtraArgument(api))
+)
 
 export default store
