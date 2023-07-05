8928f47b46c502325797535efdc941197ab86649
Andre Vargas Roo <a.vargas252@icloud.com>
2023-07-04 22:31:49 -0400

    test passing

--- src/Store/restaurants/reducers.js
+++ src/Store/restaurants/reducers.js
@@ -1,5 +1,5 @@
 import {combineReducers} from "redux"
-import { STORE_RESTAURANTS, START_LOADING } from "./actions"
+import { STORE_RESTAURANTS, START_LOADING, RECORD_LOADING_ERROR } from "./actions"
 
 function records(s = [], action) {
     switch (action.type) {
@@ -21,8 +21,13 @@ function loading(state = false, action) {
     }
 }
 
-function loadError() {
-    return true
+function loadError(state = false, action) {
+    switch (action.type) {
+        case RECORD_LOADING_ERROR:
+            return true
+        default:
+            return state
+    }
 }
 
 export default combineReducers({
