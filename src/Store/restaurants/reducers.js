5b1638617193d48cc90968f1d0fdeabf2b6057e1
Andre Vargas Roo <a.vargas252@icloud.com>
2023-07-04 21:58:24 -0400

    loading state test is passing

--- src/Store/restaurants/reducers.js
+++ src/Store/restaurants/reducers.js
@@ -10,8 +10,13 @@ function records(s = [], action) {
     }
 }
 
-function loading() {
-    return true
+function loading(state = true, action) {
+    switch (action.type) {
+        case STORE_RESTAURANTS:
+            return false
+        default:
+            return state
+    }
 }
 
 export default combineReducers({
