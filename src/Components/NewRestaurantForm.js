a4e6657159b19b151263e273edbf7c7ea3bba3aa
Andre Vargas Roo <a.vargas252@icloud.com>
2023-07-06 12:03:18 -0400

    added form to screen

---
+++ src/Components/NewRestaurantForm.js
@@ -0,0 +1,15 @@
+import { TextField } from "@mui/material/";
+
+export function NewRestaurantForm() {
+    return (
+        <form>
+            <TextField 
+                placeholder="Add Restaurant"
+                fullWidth
+                variant="filled"
+            />
+        </form>
+    )
+}
+
+export default NewRestaurantForm
