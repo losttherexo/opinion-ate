c871644aa5e6a415a8e0a032270fb2abef821e99
Andre Vargas Roo <a.vargas252@icloud.com>
2023-07-06 12:20:27 -0400

    unit test passing

--- src/Components/NewRestaurantForm.js
+++ src/Components/NewRestaurantForm.js
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import { TextField } from "@mui/material/";
 import Button from "@mui/material/Button";
 
-export function NewRestaurantForm() {
+export function NewRestaurantForm({createRestaurant}) {
+    const [name, setName] = useState('')
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        createRestaurant(name)
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <TextField 
+                value={name}
+                onChange={e => setName(e.target.value)}
                 placeholder="Add Restaurant"
                 fullWidth
                 variant="filled"
             />
-            <Button variant="contained" color="primary">
+            <Button type="submit" variant="contained" color="primary">
                 Add
             </Button>
         </form>
