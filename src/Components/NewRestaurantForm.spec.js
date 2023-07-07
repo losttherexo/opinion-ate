f1ee26b408590f158382c2633eb4267223c4a968
Andre Vargas Roo <a.vargas252@icloud.com>
2023-07-06 22:11:54 -0400

    additional test for assurance

--- src/Components/NewRestaurantForm.spec.js
+++ src/Components/NewRestaurantForm.spec.js
@@ -52,6 +52,11 @@ describe('NewRestaurant', () => {
             await fillInForm()
             expect(screen.queryByText(requiredError)).not.toBeInTheDocument()
         })
+
+        it('does not display a server error', async () => {
+            await fillInForm()
+            expect(screen.queryByText(serverError)).not.toBeInTheDocument()
+        })
     })
     describe('when empty', () => {
         async function submitEmptyForm() {
